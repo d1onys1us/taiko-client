c9a3d0aa3518698a1a1d2e2f235e4a99f7ec9bde
dave | d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2023-02-14 17:03:18 -0500

    feat(repo): implement release please

--- version/version.go
+++ version/version.go
@@ -1,26 +1,3 @@
 package version
 
-// Version info.
-var (
-	Version = "0.2.4"
-	Meta    = "dev"
-)
-
-// Git commit/date info, set via linker flags.
-var (
-	GitCommit = ""
-	GitDate   = ""
-)
-
-// VersionWithCommit returns a textual version string including Git commit/date
-// information.
-func VersionWithCommit() string {
-	vsn := Version + "-" + Meta
-	if len(GitCommit) >= 8 {
-		vsn += "-" + GitCommit[:8]
-	}
-	if (Meta != "stable") && (GitDate != "") {
-		vsn += "-" + GitDate
-	}
-	return vsn
-}
+Version = "0.2.4"
